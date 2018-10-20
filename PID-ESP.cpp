a1838de9cbbc42d3a14573a91bc599734242d740
stupid <arjungandhi06@gmail.com>
2018-10-20 18:06:45 -0400

    Created .h and .cpp and updated liscence info

---
+++ PID-ESP.cpp
@@ -0,0 +1,8 @@
+/***************************************************************************
+  This is a library for PID control
+  Support me by giving me waffles
+  www.arjungandhi.com
+  Also will accept hugs
+  Written By Arjun Gandhi
+  MIT license, all text above must be included in any redistribution
+ **************************************************************************/
---
+++ PID-ESP.h
@@ -0,0 +1,8 @@
+/***************************************************************************
+  This is a library for PID control
+  Support me by giving me waffles
+  www.arjungandhi.com
+  I also take cookies
+  Written By Arjun Gandhi
+  MIT license, all text above must be included in any redistribution
+ ***************************************************************************/
