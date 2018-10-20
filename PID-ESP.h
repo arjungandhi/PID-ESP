06330c780b8aed771fbf5e4b5b56ca0511d0a51c
stupid <arjungandhi06@gmail.com>
2018-10-20 18:11:32 -0400

    yup

--- PID-ESP.h
+++ PID-ESP.h
@@ -2,7 +2,7 @@
   This is a library for PID control
   Support me by giving me waffles
   www.arjungandhi.com
-  I also take cookies
+  I also take hugs
   Written By Arjun Gandhi
   MIT license, all text above must be included in any redistribution
  ***************************************************************************/
