6fb1eda23885a45987112fc909c3214f4501744e
stupid <arjungandhi06@gmail.com>
2018-10-20 18:11:19 -0400

    im just procrastinating on actually writing code

--- PID-ESP.cpp
+++ PID-ESP.cpp
@@ -2,7 +2,7 @@
   This is a library for PID control
   Support me by giving me waffles
   www.arjungandhi.com
-  Also will accept hugs
+  I also take hugs
   Written By Arjun Gandhi
   MIT license, all text above must be included in any redistribution
- **************************************************************************/
+ ***************************************************************************/
