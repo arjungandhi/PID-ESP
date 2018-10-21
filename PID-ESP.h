4675c4a4d17252d50b1995ad7ac178f32e77585b
stupid <arjungandhi06@gmail.com>
2018-10-21 13:06:46 -0400

    started basic .h stuff

--- PID-ESP.h
+++ PID-ESP.h
@@ -6,3 +6,22 @@
   Written By Arjun Gandhi
   MIT license, all text above must be included in any redistribution
  ***************************************************************************/
+#ifndef PID_H
+#define PID_H
+
+class PID {
+
+  public:
+
+    PID(double max,double min, double Kp, double Ki, double Kd);
+    double calculate(double target, double error);
+    void setMax(double value);
+    void setMin(double value);
+    void setKp(double value);
+    void setKi(double value);
+    void setKd(double value);
+
+
+};
+
+#endif
