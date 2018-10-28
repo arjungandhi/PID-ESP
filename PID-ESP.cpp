1565f48ea224c396424986284cdc834dc9befc8c
arjungandhi <arjungandhi06@gmail.com>
2018-10-28 19:42:02 -0400

    set up cpp constructor along with calculate and P,I,D calculation methods

--- PID-ESP.cpp
+++ PID-ESP.cpp
@@ -6,4 +6,51 @@
   Written By Arjun Gandhi
   MIT license, all text above must be included in any redistribution
  ***************************************************************************/
-#include <PID-ESP.h>
+#include <Arduino.h>
+#include "PID-ESP.h"
+
+/*Constructor (...)*********************************************************
+ *    The parameters spec
+ ***************************************************************************/
+
+PID::PID(double max,double min, double kp, double ki, double kd,unsigned long startTime){
+  setLimits(min, max);
+  setKp(kd);
+  setKi(ki);
+  setKd(kd);
+  setStartTime(startTime);
+  prevError=0; //first time sets previous error to 0 cause duh
+  integral=0;
+}
+
+/* Public Methods (...)*********************************************************
+ *    These are the methods for every one to play with
+ ***************************************************************************/
+
+double PID::calculate(double error){
+    long timeChange=millis()-previousTime;
+    previousTime=millis();
+    return calculateP(error)+calculateI(error,timeChange)+calculateD(error,timeChange);
+
+}
+
+
+/* Public Methods (...)*********************************************************
+ *    These methods are for my eyes only
+ ***************************************************************************/
+double PID::calculateP(double error){
+  return kp*error;
+}
+
+double PID::calculateI(double error,long timeChange){
+  integral +=(double) error * timeChange;
+  return ki*integral;
+}
+
+double PID::calculateD(double error,long timeChange){
+  derivative =(double) (error-prevError)/timeChange;
+  prevError=error;
+  return kd*derivative;
+
+
+}
--- PID-ESP.h
+++ PID-ESP.h
@@ -13,23 +13,31 @@ class PID {
 
   public:
 
-    PID(double max,double min, double kp, double ki, double kd);
-    double calculate(double target, double error);
+    PID(double max,double min, double kp, double ki, double kd,unsigned long startTime);
+    double calculate(double error);
     void setLimits(double min,double max);
     void setKp(double value);
     void setKi(double value);
     void setKd(double value);
+    void setStartTime(double value);
 
     double getKp();
     double getKi();
     double getKd();
 
   private:
+    double calculateP(double error);
+    double calculateI(double error,long timeChange);
+    double calculateD(double error,long timeChange);
     double max;
     double min;
     double kp;
     double ki;
     double kd;
+    double integral;
+    double derivative;
+    long previousTime;
+    double prevError;
 
 
 
